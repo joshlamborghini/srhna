7e15e6609d4e4219c28f927ceeee352f09e20755
Josh <jdlamborghini@gmail.com>
2014-12-04 15:32:29 -0500

    worked

--- srhna/src/test/java/com/srhna/app/Import.java
+++ srhna/src/test/java/com/srhna/app/Import.java
@@ -13,9 +13,20 @@ public class Import {
 	            BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\jl79\\Documents\\GitHub\\srhna\\srhna\\src\\test\\java\\com\\srhna\\app\\test.txt"));
 	            String str;
 	            str = in.readLine();
+	            
+	            
+	            String[] parts = str.split(",");
+	            
 	            while ((str = in.readLine()) != null) {
-	                System.out.println(str);
+	            	
+		            for (int i = 0; i < parts.length; i++) {
+						System.out.print(parts[i]);
+						System.out.print(" ");
+					}
+		            parts = str.split(",");
+		            System.out.println();
 	            }
+	            
 	            in.close();
 	        } catch (IOException e) {
 	            System.out.println("File Read Error");
